182e2d47969b05987c87db49f48349245a461cf6
hclee <hclee@dataeum.kr>
2022-09-23 13:53:43 +0900

    core > domain > post 추가중

---
+++ src/main/java/com/heech/heechlog/core/domain/Post.java
@@ -0,0 +1,25 @@
+package com.heech.heechlog.core.domain;
+
+import com.heech.heechlog.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+}
