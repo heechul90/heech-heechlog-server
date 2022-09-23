908cd783562cf6a3597822272b7473826ebdf2ad
hclee <hclee@dataeum.kr>
2022-09-23 13:38:54 +0900

    heechlog project source

---
+++ src/main/java/com/heech/heechlog/HeechlogApplication.java
@@ -0,0 +1,13 @@
+package com.heech.heechlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeechlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeechlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heech/heechlog/HeechlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heech.heechlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeechlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
