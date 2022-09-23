4d150d8a348ad6edf804a7efd4f9f1a09ed4750c
hclee <hclee@dataeum.kr>
2022-09-23 13:59:53 +0900

    core > controller 추가

---
+++ src/main/java/com/heech/heechlog/core/controller/PostController.java
@@ -0,0 +1,35 @@
+package com.heech.heechlog.core.controller;
+
+import com.heech.heechlog.core.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    /**
+     * Post 목록 조회
+     */
+
+    /**
+     * Post 단건 조회
+     */
+
+    /**
+     * Post 저장
+     */
+
+    /**
+     * Post 수정
+     */
+
+    /**
+     * Post 삭제
+     */
+
+}
